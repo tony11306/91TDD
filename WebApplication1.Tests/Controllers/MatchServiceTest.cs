aa2f83b28dbc06713b722c2f18485d835639ba21
tony.xu <tony.xu@titansoft.com.sg>
2025-07-24 13:01:15 +0800

    enhance MatchService to handle AwayGoal, NextPeriod, HomeCancel, and AwayCancel events; add corresponding unit tests

--- WebApplication1.Tests/Controllers/MatchServiceTest.cs
+++ WebApplication1.Tests/Controllers/MatchServiceTest.cs
@@ -34,4 +34,106 @@ public class MatchServiceTest
         _matchRepository.Received(1).UpdateMatchResult(matchId, "H");
         result.Should().Be("H");
     }
+
+    [Test]
+    public void awaygoal()
+    {
+        // Arrange
+        var matchId = 1;
+        var initialResult = "";
+        _matchRepository.GetMatchResultById(matchId).Returns(initialResult);
+
+        // Act
+        var result = _matchService.UpdateMatchResult(matchId, MatchEvent.AwayGoal);
+
+        // Assert
+        _matchRepository.Received(1).GetMatchResultById(matchId);
+        _matchRepository.Received(1).UpdateMatchResult(matchId, "A");
+        result.Should().Be("A");
+    }
+
+    [Test]
+    public void nextperiod()
+    {
+        // Arrange
+        var matchId = 1;
+        var initialResult = "A";
+        _matchRepository.GetMatchResultById(matchId).Returns(initialResult);
+
+        // Act
+        var result = _matchService.UpdateMatchResult(matchId, MatchEvent.NextPeriod);
+
+        // Assert
+        _matchRepository.Received(1).GetMatchResultById(matchId);
+        _matchRepository.Received(1).UpdateMatchResult(matchId, "A;");
+        result.Should().Be("A;");
+    }
+
+    [Test]
+    public void homecancel()
+    {
+        // Arrange
+        var matchId = 1;
+        var initialResult = "AH";
+        _matchRepository.GetMatchResultById(matchId).Returns(initialResult);
+
+        // Act
+        var result = _matchService.UpdateMatchResult(matchId, MatchEvent.HomeCancel);
+
+        // Assert
+        _matchRepository.Received(1).GetMatchResultById(matchId);
+        _matchRepository.Received(1).UpdateMatchResult(matchId, "A");
+        result.Should().Be("A");
+    }
+
+    [Test]
+    public void homecancel_with_semicolon()
+    {
+        // Arrange
+        var matchId = 1;
+        var initialResult = "AH;";
+        _matchRepository.GetMatchResultById(matchId).Returns(initialResult);
+
+        // Act
+        var result = _matchService.UpdateMatchResult(matchId, MatchEvent.HomeCancel);
+
+        // Assert
+        _matchRepository.Received(1).GetMatchResultById(matchId);
+        _matchRepository.Received(1).UpdateMatchResult(matchId, "A;");
+        result.Should().Be("A;");
+    }
+
+    [Test]
+    public void awaycancel()
+    {
+        // Arrange
+        var matchId = 1;
+        var initialResult = "HA";
+        _matchRepository.GetMatchResultById(matchId).Returns(initialResult);
+
+        // Act
+        var result = _matchService.UpdateMatchResult(matchId, MatchEvent.AwayCancel);
+
+        // Assert
+        _matchRepository.Received(1).GetMatchResultById(matchId);
+        _matchRepository.Received(1).UpdateMatchResult(matchId, "H");
+        result.Should().Be("H");
+    }
+
+    [Test]
+    public void awaycancel_with_semicolon()
+    {
+        // Arrange
+        var matchId = 1;
+        var initialResult = "HA;";
+        _matchRepository.GetMatchResultById(matchId).Returns(initialResult);
+
+        // Act
+        var result = _matchService.UpdateMatchResult(matchId, MatchEvent.AwayCancel);
+
+        // Assert
+        _matchRepository.Received(1).GetMatchResultById(matchId);
+        _matchRepository.Received(1).UpdateMatchResult(matchId, "H;");
+        result.Should().Be("H;");
+    }
 }
--- WebApplication1/Controllers/MatchService.cs
+++ WebApplication1/Controllers/MatchService.cs
@@ -16,6 +16,32 @@ public class MatchService(IMatchRepository matchRepository) : IMatchService
         if (matchEvent == MatchEvent.HomeGoal)
         {
             matchResult += 'H';
+        } else if (matchEvent == MatchEvent.AwayGoal)
+        {
+            matchResult += 'A';
+        } else if (matchEvent == MatchEvent.NextPeriod)
+        {
+            matchResult += ';';
+        } else if (matchEvent == MatchEvent.HomeCancel)
+        {
+            if (matchResult.Length > 0 && matchResult[^1] == 'H')
+            {
+                matchResult = matchResult[..^1]; // Remove last 'H'
+            } else if (matchResult.Length > 0 && matchResult[^1] == ';' && matchResult.Length > 1 && matchResult[^2] == 'H')
+            {
+                // remove last 'H' before ';'
+                matchResult = matchResult[..^2] + ';';
+            }
+        } else if (matchEvent == MatchEvent.AwayCancel)
+        {
+            if (matchResult.Length > 0 && matchResult[^1] == 'A')
+            {
+                matchResult = matchResult[..^1]; // Remove last 'A'
+            } else if (matchResult.Length > 0 && matchResult[^1] == ';' && matchResult.Length > 1 && matchResult[^2] == 'A')
+            {
+                // remove last 'A' before ';'
+                matchResult = matchResult[..^2] + ';';
+            }
         }
 
         matchRepository.UpdateMatchResult(matchId, matchResult);
